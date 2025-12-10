03f68878acdfb0d4988281608e13c974d1ef71bc
Jan Niklas Hasse <jhasse@bixense.com>
2025-12-10 21:12:46 +0100

    Use README.md on PyPI, fix #2

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
+from pathlib import Path
+
 from setuptools import setup
 
-VERSION='0.1.1' # also see __init__.py
+VERSION = "0.2.0"  # also see __init__.py
+
+README = Path(__file__).parent / "README.md"
+long_description = README.read_text(encoding="utf-8")
 
 setup(
-    name='lus',
+    name="lus",
     version=VERSION,
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
     url="https://bixense.com/lus",
-    download_url='https://github.com/jhasse/lus/archive/v{}.tar.gz'.format(VERSION),
+    download_url="https://github.com/jhasse/lus/archive/v{}.tar.gz".format(VERSION),
     description="A simple task-runner using KDL for configuration",
-    packages=['lus'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["lus"],
     entry_points={
-        'console_scripts': ['lus = lus:main'],
+        "console_scripts": ["lus = lus:main"],
     },
     install_requires=[
-        'ckdl',
-        'expandvars',
+        "ckdl",
+        "expandvars",
     ],
 )
