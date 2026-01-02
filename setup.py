068402ad2070bd5308acdf8592db03b74298c8a8
Jan Niklas Hasse <jhasse@bixense.com>
2026-01-02 12:41:08 +0100

    Bump version to 0.2.1 and update project URL to GitHub

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from setuptools import setup
 
-VERSION = "0.2.0"  # also see __init__.py
+VERSION = "0.2.1"  # also see __init__.py
 
 README = Path(__file__).parent / "README.md"
 long_description = README.read_text(encoding="utf-8")
@@ -12,7 +12,7 @@ setup(
     version=VERSION,
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
-    url="https://bixense.com/lus",
+    url="https://github.com/jhasse/lus",
     download_url="https://github.com/jhasse/lus/archive/v{}.tar.gz".format(VERSION),
     description="A simple task-runner using KDL for configuration",
     long_description=long_description,
